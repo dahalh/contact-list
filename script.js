b12c40e281153bb80b1df263f70443625199459b
dahalh <dahalh@outlook.com>
2022-02-21 16:19:08 +1100

    added search function

--- script.js
+++ script.js
@@ -1,26 +1,15 @@
 const listElm = document.querySelector("#list");
-const searchBar = document.getElementById("searchBar");
-
 const apiUrl = "https://randomuser.me/api/?";
+const userCountElm = document.querySelector("#user-count");
 
+let userArgs = [];
 
+const displayUser = (args = userArgs) => {
+  let str = "";
 
-const fetchUsers = (params = "results=20") => {
-    fetch(apiUrl + params)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        
-        //data is array
-        const user = data.results;
-        let str = "";
-        
-
-        user.map(usr =>{
-            console.log(usr);
-
-            str += `
-        <div class="col-md-6 col-lg-3 py-3">
+  args.map(usr => {
+    str += `
+    <div class="col-md-6 col-lg-3 py-3">
                     <div class="card">
                         <img src="${usr.picture.large}" class="card-img-top" alt="...">
                         <div class="card-body">
@@ -41,24 +30,52 @@ const fetchUsers = (params = "results=20") => {
                         </div>
                       </div>
                 </div>
-        
-        `;
-        });
+    `;
+  });
 
-        listElm.innerHTML = str;
+  userCountElm.innerHTML = args.length;
+  listElm.innerHTML = str;
+};
+
+
+const fetchUsers = (params = "results=20") => {
+    fetch(apiUrl + params)
+    .then(response => response.json())
+    .then(data => {
+        // const user = data.results;
+        userArgs = data.results;
+        displayUser();
+
     })
-    .catch(err => (listElm.innerHTML = `<div class="alert alert-danger" role="alert">
+    .catch(err => {listElm.innerHTML = `<div class="alert alert-danger" role="alert">
     Oops! Something went wrong.
-  </div>`));
+  </div>`});
 };
 
 fetchUsers();
 
 const handleOnChange = e => {
     const params = "results=20&gender=" + e.value;
+
     fetchUsers(params);
 };
 
+const handleOnSearch = () => {
+  const searchStr = document.getElementById("search").value;
+
+  const filteredUser = userArgs.filter(item => {
+    const userName = `${item?.name?.first} ${item?.name?.last}`;
+    console.log(userName, searchStr);
+
+    if (userName.toLowerCase().includes(searchStr.toLowerCase())) {
+      return item;
+    }
+
+  });
+
+  displayUser(filteredUser);
+  console.log(filteredUser);
+};
  
 
 
